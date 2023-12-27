0fe025b2d3fe0220b967ace5f0e19c5a6b050c66
winrid <winrid@gmail.com>
2023-12-26 18:26:55 -0800

    upad

--- netpanzer/settings.py
+++ netpanzer/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+    dotenv.read_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
