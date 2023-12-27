1a5d384d8854900870836e2cdb2447d1e5618e5d
winrid <winrid@gmail.com>
2023-12-26 18:01:26 -0800

    use 3.11

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # The orchestrator runs this file post-deployment. It is not for usage locally!
 
-python3 -m venv env
+python3.11 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 
-python3 manage.py migrate
-python3 manage.py collectstatic --skip-checks --no-input
+python3.11 manage.py migrate
+python3.11 manage.py collectstatic --skip-checks --no-input
 /home/winrid/np-website/env/bin/gunicorn \
           --access-logfile - \
           --error-logfile - \
