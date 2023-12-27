8129a04cfa4982e69787769066663b04e1ecc9da
winrid <winrid@gmail.com>
2023-12-26 17:03:13 -0800

    setup env file

--- netpanzer/settings.py
+++ netpanzer/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bq@6x+*tbwgti+p5$%-x7=xyt5a10*(9ad*ws^tw=0f2-u3kgx'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = 'DEBUG' in os.environ and os.environ['DEBUG'] == 'True'
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
