a8fddb8cd7feef8367e776b71804c0200e0bb1cd
winrid <winrid@gmail.com>
2023-12-28 11:43:41 -0800

    fixing static file location

--- netpanzer/settings.py
+++ netpanzer/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'netpanzer' / 'static',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
